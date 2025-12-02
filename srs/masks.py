93163f2bbee6009c20270681361c044880cf0d87
Дмитрий Глоба <dima.globa2015@yandex.ru>
2025-12-02 13:55:26 +0800

    start

---
+++ srs/masks.py
@@ -0,0 +1,19 @@
+def get_mask_card_number(card_number: int) -> str:
+    """Функция принимает на вход номер карты и возвращает её маску"""
+    card_number_string = str(card_number)
+    card_number_list = list(card_number_string)
+    card_number_list[6:12] = ["*", "*", "*", "*", "*", "*"]
+    card_number_list.insert(4, " ")
+    card_number_list.insert(9, " ")
+    card_number_list.insert(14, " ")
+    card_number_string_new = "".join(card_number_list)
+    return card_number_string_new
+
+
+def get_mask_account(account: int) -> str:
+    """Функция принимает на вход номер счета и возвращает его маску"""
+    account_string = str(account)
+    account_list = list(account_string)
+    account_list[:-4] = ["*", "*"]
+    account_list_new = "".join(account_list)
+    return account_list_new
