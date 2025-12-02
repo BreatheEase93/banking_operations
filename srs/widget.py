b3df31173ca32ccfe20b8ff7218645dd5f277785
Дмитрий Глоба <dima.globa2015@yandex.ru>
2025-12-02 21:38:14 +0800

    flake8,blak,mypy

--- srs/widget.py
+++ srs/widget.py
@@ -1,16 +1,18 @@
-import masks
+import srs.masks
+
 
 def mask_account_card(account_card: str) -> str:
     """Функция принимающяя тип и номер карты или счета, возвращать строку с замаскированным номером"""
     account_card_split = account_card.split()
     for i, word in enumerate(account_card_split):
-       if word.isdigit():
-           if len(word) == 20:
-               account_card_split[i] = masks.get_mask_account(int(word))
-           if len(word) == 16:
-               account_card_split[i] = masks.get_mask_card_number(int(word))
-    account_card = ' '.join(account_card_split)
+        if word.isdigit():
+            if len(word) == 20:
+                account_card_split[i] = srs.masks.get_mask_account(int(word))
+            if len(word) == 16:
+                account_card_split[i] = srs.masks.get_mask_card_number(int(word))
+    account_card = " ".join(account_card_split)
     return account_card
+
 
 def get_date(date: str) -> str:
     """Меняет формат даты"""
