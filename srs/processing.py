8ec582ca154adf3eff7972845171879239f9190a
Дмитрий Глоба <dima.globa2015@yandex.ru>
2025-12-03 13:37:41 +0800

    проверка flake8, black, mypy, isort

--- srs/processing.py
+++ srs/processing.py
@@ -1,17 +1,16 @@
 from typing import Any
 
 
-def filter_by_state(by_state: list[dict[str, Any]], state: str="EXECUTED") -> list[dict[str, Any]]:
+def filter_by_state(by_state: list[dict[str, Any]], state: str = "EXECUTED") -> list[dict[str, Any]]:
     """Функция возвращает новый список словарей, содержащий только те словари, у которых ключ state"""
-    new_by_state:   list[dict[str, Any]] =[]
+    new_by_state: list[dict[str, Any]] = []
     for item in by_state:
-      if item.get("state") == state:
+        if item.get("state") == state:
             new_by_state.append(item)
     return new_by_state
 
 
-def sort_by_date(by_state: list[dict[str, Any]], decreasing: bool= True) -> list[dict[str, Any]]:
+def sort_by_date(by_state: list[dict[str, Any]], decreasing: bool = True) -> list[dict[str, Any]]:
     """Функция возвращает новый список, отсортированный по дате (date)."""
-    sorted_by_date: list[dict[str, Any]] = sorted(by_state, key=lambda x: x['date'], reverse=decreasing)
+    sorted_by_date: list[dict[str, Any]] = sorted(by_state, key=lambda x: x["date"], reverse=decreasing)
     return sorted_by_date
-
