efd7522c60927898825b57db42d1ee4f922ab97f
Дмитрий Глоба <dima.globa2015@yandex.ru>
2026-01-28 14:39:41 +0800

    add csv, xlsx in data

---
+++ srs/utils_crv_xlsx.py
@@ -0,0 +1,18 @@
+import json
+import os
+from typing import Any, Dict, List
+
+
+def call_word_list(json_file: str) -> List[Dict[str, Any]]:
+    """Функция, которая принимает на вход путь до crv и
+    возвращает список словарей с данными о финансовых транзакциях."""
+    empty_list: list = []
+    if not os.path.exists(json_file):
+        return empty_list
+    if not json_file.lower().endswith(".crv"):
+        return empty_list
+    with open(json_file, "r", encoding="utf-8") as f:
+        data = json.load(f)
+        if not isinstance(data, list):
+            return empty_list
+    return data
